1e7162d4f48d56ed9c3650d7d2aa2c0a03f9efb2
Mhtsu <friplevel@gmail.com>
2019-11-21 01:32:33 +0900

    Unityサンプルプロジェクト追加

---
+++ sample/roll-a-ball-tutrial/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //入力を取得（横はx軸,縦はz軸）
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(x*speed, 0, z*speed);
+    }
+
+}
